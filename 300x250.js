f4bc55c7aff1ef75be8b540f26c5dc0423c822fb
olivialuz <89439795+olivialuz@users.noreply.github.com>
2021-09-04 21:29:09 +0700

    Create 300x250.js

---
+++ 300x250.js
@@ -0,0 +1,9 @@
+
+	atOptions = {
+		'key' : '5f17921dd7e119be59257162caccb5ba',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://breakdownreprintsentimental.com/5f17921dd7e119be59257162caccb5ba/invoke.js"></scr' + 'ipt>');
